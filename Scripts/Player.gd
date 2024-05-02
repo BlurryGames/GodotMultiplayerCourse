3248c7fc60018081fb4729dee2bfa11d4967d330
Blurry <julian.lb.games@gmail.com>
2024-05-02 22:03:09 +0200

    Section2: Player Character / Lesson10: Player Refactoring

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -16,12 +16,10 @@ var jumpCount: int = 0
 var cameraInstance: Camera2D = null
 
 func _ready()-> void:
-	cameraInstance = playerCamera.instantiate()
-	cameraInstance.global_position.y = cameraHeight
-	get_tree().current_scene.add_child.call_deferred(cameraInstance)
+	setUpCamera()
 
 func _process(_delta)-> void:
-	cameraInstance.global_position.x = global_position.x
+	updateCameraPosition()
 
 func _physics_process(_delta: float)-> void:
 	var horizontalInput: float = (
@@ -31,26 +29,38 @@ func _physics_process(_delta: float)-> void:
 	velocity.x = horizontalInput * movementSpeed
 	velocity.y += gravity
 	
+	handleMovementState()
+	
+	move_and_slide()
+	
+	faceMovementDirection(horizontalInput)
+
+
+func _on_animated_sprite_2d_animation_finished():
+	playerSprite.play("Jump")
+
+func faceMovementDirection(horizontalInput: float)-> void:
+	if not is_zero_approx(horizontalInput):
+		if horizontalInput < 0:
+			playerSprite.scale = Vector2(-initialSpriteScale.x, initialSpriteScale.y)
+		else:
+			playerSprite.scale = initialSpriteScale
+
+func setUpCamera()-> void:
+	cameraInstance = playerCamera.instantiate()
+	cameraInstance.global_position.y = cameraHeight
+	get_tree().current_scene.add_child.call_deferred(cameraInstance)
+
+func updateCameraPosition()-> void:
+	cameraInstance.global_position.x = global_position.x
+
+func handleMovementState()-> void:
 	var isFalling: bool = velocity.y > 0 and not is_on_floor()
 	var isJumping: bool = Input.is_action_just_pressed("Jump") and is_on_floor()
 	var isDoubleJumping: bool = Input.is_action_just_pressed("Jump") and isFalling
 	var isJumpCancelled: bool = Input.is_action_just_released("Jump") and velocity.y < 0.0
 	var isIdle: bool = is_on_floor() and is_zero_approx(velocity.x)
 	var isWalking: bool = is_on_floor() and not is_zero_approx(velocity.x)
-	
-	if isJumping:
-		jumpCount += 1
-		velocity.y = -jumpStrength
-	elif isDoubleJumping:
-		jumpCount += 1
-		if jumpCount <= maxJumps:
-			velocity.y = -jumpStrength
-	elif isJumpCancelled:
-		velocity.y = 0.0
-	elif is_on_floor():
-		jumpCount = 0
-	
-	move_and_slide()
 	
 	if isJumping:
 		playerSprite.play("JumpStart")
@@ -63,11 +73,14 @@ func _physics_process(_delta: float)-> void:
 	elif isIdle:
 		playerSprite.play("Idle")
 	
-	if not is_zero_approx(horizontalInput):
-		if horizontalInput < 0:
-			playerSprite.scale = Vector2(-initialSpriteScale.x, initialSpriteScale.y)
-		else:
-			playerSprite.scale = initialSpriteScale
-
-func _on_animated_sprite_2d_animation_finished():
-	playerSprite.play("Jump")
+	if isJumping:
+		jumpCount += 1
+		velocity.y = -jumpStrength
+	elif isDoubleJumping:
+		jumpCount += 1
+		if jumpCount <= maxJumps:
+			velocity.y = -jumpStrength
+	elif isJumpCancelled:
+		velocity.y = 0.0
+	elif is_on_floor():
+		jumpCount = 0
