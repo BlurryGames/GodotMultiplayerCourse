5c55b617818fc08929c62c48759c1412fe925e21
Blurry <julian.lb.games@gmail.com>
2024-05-03 16:08:30 +0200

    Section3: Multiplayer Conection / Lesson7: Spawning Players

---
+++ Scripts/Level.gd
@@ -0,0 +1,43 @@
+class_name Level extends Node2D
+
+@export var playerScene: PackedScene = null
+@export var playersContainer: Node2D = null
+@export var spawnPoints: Array[Node2D] = []
+
+var nextSpawnPointIndex: int = 0
+
+func _ready()-> void:
+	if not multiplayer.is_server():
+		return
+	
+	multiplayer.peer_disconnected.connect(deletePlayer)
+	
+	for id in multiplayer.get_peers():
+		addPlayer(id)
+	
+	addPlayer(1)
+
+func _exit_tree()-> void:
+	if not multiplayer.is_server():
+		return
+	
+	multiplayer.peer_disconnected.disconnect(deletePlayer)
+
+func addPlayer(id: int)-> void:
+	var playerInstance: CharacterBody2D = playerScene.instantiate()
+	playerInstance.position = getSpawnPoint()
+	playerInstance.name = str(id)
+	playersContainer.add_child(playerInstance)
+
+func deletePlayer(id: int)-> void:
+	if not playersContainer.has_node(str(id)):
+		return
+	
+	playersContainer.get_node(str(id)).queue_free()
+
+func getSpawnPoint()-> Vector2:
+	var spawnPoint: Vector2 = spawnPoints[nextSpawnPointIndex].position
+	nextSpawnPointIndex += 1
+	if nextSpawnPointIndex >= len(spawnPoints):
+		nextSpawnPointIndex = 0
+	return spawnPoint
