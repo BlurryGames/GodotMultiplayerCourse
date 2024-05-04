a0b6f479b22a88eb728b295daa3e41bde438a191
Blurry <julian.lb.games@gmail.com>
2024-05-04 22:58:19 +0200

    Section4: Platformer Puzzles / Lesson4: Opening A Door

---
+++ Scripts/Door.gd
@@ -0,0 +1,21 @@
+class_name Door extends Node2D
+
+@export var isOpen: bool = false
+@export var doorOpen: Sprite2D = null
+@export var doorClosed: Sprite2D = null
+@export var collider: CollisionShape2D = null
+
+func _on_multiplayer_synchronizer_synchronized():
+	setDoorProperties()
+
+func activate(state: bool)-> void:
+	if not multiplayer.is_server():
+		return
+	
+	isOpen = state
+	setDoorProperties()
+
+func setDoorProperties()-> void:
+	doorOpen.visible = isOpen
+	doorClosed.visible = !isOpen
+	collider.set_deferred("disabled", isOpen)
